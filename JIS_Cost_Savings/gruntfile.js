6af7996c6e4e8a3ebe3d6b99966f6b8d194774c6
Bob Schnable <rschnable@saberlogic.com>
2016-02-24 07:50:07 -0500

    Finished grunt task for update in browser;

--- JIS_Cost_Savings/gruntfile.js
+++ JIS_Cost_Savings/gruntfile.js
@@ -1,39 +1,16 @@
 ﻿/// <binding />
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         watch: {
-            refresh: {
-                options: {
-                    livereload: true
-                },
-                files : ['Views/Home/Index.cshtml', 'Views/Home/*']
-            }
-        },
-        connect: {
-            options: {
-                hostname: 'localhost',
-                directory: 'Views/Home/',
-                livereload: 35729 //62569
+            options:{
+                livereload: true
             },
-            livereload: {
-                options: {
-                    directory: 'Views/Home/',
-                    open: true
-                },
-                files: [
-                    {
-                        expand: true,
-                        src: ['~/Views/Home/**'],
-                        dest: 'wwwroot'
-                    }
-                ]
-            }
+            files: ['Views/Home/Index.cshtml', 'Views/Home/*'],
         }
     });
 
+    grunt.loadNpmTasks('grunt-livereload');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-
-    grunt.registerTask('default', ['connect:livereload', 'watch']);
+    
+    grunt.registerTask('default', 'watch');
 }
